7134aefc6fee5954309edce01ee31632c05b8665
mblough <mblough7@gmail.com>
2015-01-27 20:28:30 -0500

    Integrated Angular into the rail app

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,15 @@
+angular.module('angularRails', ['ui.router', 'templates'])
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+	$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: 'home/_home.html',
+			controller: 'MainCtrl'
+		})
+		.state('posts', {
+			url: '/posts/{id}',
+			templateUrl: 'posts/_posts.html',
+			controller: 'PostsCtrl'
+		});
+	$urlRouterProvider.otherwise('home');
+}]);
